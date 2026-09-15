98f1d4594d9a8955091623cb9147b48eb0fdbf8d
jourdese <jourdesepalacio@gmail.com>
2026-09-15 18:32:00 +0800

    Assemble high-resolution Rib Crib interior asset

---
+++ frontend/lib/rib-crib/assets/interior.ts
@@ -0,0 +1,5 @@
+import interior0 from './interior-0';
+import interior1 from './interior-1';
+import interior2 from './interior-2';
+
+export const RIB_INTERIOR = 'data:image/webp;base64,' + [interior0, interior1, interior2].join('');
