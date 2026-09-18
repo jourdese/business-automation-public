17e85c3bb14e016a2aa5c5bcf7983033fb9ffedb
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:22 +0800

    feat: add Command Center forecast route

---
+++ frontend/app/command-center/[businessId]/forecast/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterForecastPage() {
+  return <CommandCenterSectionView section="forecast" />;
+}
