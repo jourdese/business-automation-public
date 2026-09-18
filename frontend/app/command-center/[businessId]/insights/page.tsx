e14fb5037afff7151486e7bf9337816eb9b52ee2
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:49 +0800

    feat: add Command Center insights route

---
+++ frontend/app/command-center/[businessId]/insights/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterInsightsPage() {
+  return <CommandCenterSectionView section="insights" />;
+}
