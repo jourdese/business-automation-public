932987d2b3933e0dccee95358750e45d3ef13dd9
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:17 +0800

    feat: add Command Center overview route

---
+++ frontend/app/command-center/[businessId]/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterOverviewPage() {
+  return <CommandCenterSectionView section="overview" />;
+}
