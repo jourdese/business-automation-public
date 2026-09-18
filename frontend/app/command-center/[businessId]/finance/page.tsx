28cd4dec7ee85fcdea94b90b4baeaf25ce767d4b
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:34 +0800

    feat: add Command Center finance route

---
+++ frontend/app/command-center/[businessId]/finance/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterFinancePage() {
+  return <CommandCenterSectionView section="finance" />;
+}
