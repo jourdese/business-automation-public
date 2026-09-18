2d51aac908cab9cd15e17deecca8fa4d75ef6abc
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:44 +0800

    feat: add Command Center briefings route

---
+++ frontend/app/command-center/[businessId]/briefings/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterBriefingsPage() {
+  return <CommandCenterSectionView section="briefings" />;
+}
