0e8ceb50fcb0f8712bbd17c9d82fb9c6f05d1fdf
jourdese <jourdesepalacio@gmail.com>
2026-09-19 04:31:30 +0800

    feat: add Command Center performance route

---
+++ frontend/app/command-center/[businessId]/performance/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterPerformancePage() {
+  return <CommandCenterSectionView section="performance" />;
+}
