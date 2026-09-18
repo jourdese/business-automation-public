9f0bd69f4b84c3be59095cb8982391f9352bd8ba
jourdese <jourdesepalacio@gmail.com>
2026-09-19 05:16:22 +0800

    feat: add Command Center activity audit route

---
+++ frontend/app/command-center/[businessId]/activity/page.tsx
@@ -0,0 +1,5 @@
+import CommandCenterSectionView from "@/components/command-center/CommandCenterSectionView";
+
+export default function CommandCenterActivityPage() {
+  return <CommandCenterSectionView section="activity" />;
+}
